d32e5c630dde538d0f2d4d2d76c73765b8d81e71
IprahimTurgut1 <iprahim.turgut@ensea.fr>
2025-12-21 18:42:50 +0100

    Update question2.c

--- question2.c
+++ question2.c
@@ -9,11 +9,11 @@
 #include "question5.h"
 
 /*
- * La fonction executeOneCommand(long *execute_time)
- * Lit une commande simple saisie par l’utilisateur, sans gestion des arguments.
- * La commande est exécutée dans un processus fils créé avec fork().
- * Le processus père attend la fin de l’exécution, récupère le statut
- * et mesure le temps d’exécution de la commande.
+ * The function executeOneCommand(long *execute_time)
+ * Reads a simple command entered by the user, without argument handling.
+ * The command is executed in a child process created with fork().
+ * The parent process waits for the execution to finish, retrieves the status,
+ * and measures the command's execution time.
  */
 
 int executeOneCommand(long *execute_time){
