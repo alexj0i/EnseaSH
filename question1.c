f3274d3d52813a82703b8a990ab0a1a2b682907f
IprahimTurgut1 <iprahim.turgut@ensea.fr>
2025-12-21 17:24:30 +0100

    Update question1.c

--- question1.c
+++ question1.c
@@ -9,14 +9,23 @@
 #define WELCOME_MESSAGE "Bienvenue dans le Shell ENSEA.\nPour quitter, taper 'exit'.\n"
 #define PROMPT_MESSAGE "enseash % "
 
+/*
+ * La fontion welcomePrompt()
+ * Affiche le message d’accueil du shell lors de son lancement.
+ * L’affichage est réalisé avec write afin de respecter les contraintes du projet.
+*/
+
 void welcomePrompt(){
-
     write(STDOUT_FILENO,WELCOME_MESSAGE,strlen(WELCOME_MESSAGE));
-
 }
 
-void regularPrompt(){
+/*
+ * La fonction regularPrompt()
+ * Affiche le prompt simple du shell pour inviter l’utilisateur à saisir une commande.
+ * En cas d’erreur lors de l’écriture sur la sortie standard, le programme est arrêté.
+*/
 
+void regularPrompt(){
     int status = write(STDOUT_FILENO,PROMPT_MESSAGE,strlen(PROMPT_MESSAGE));
     if (status == -1){
         exit(EXIT_FAILURE);
