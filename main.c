acace28eae5863a1d2c8e6df8dc55d3664de1bc7
IprahimTurgut1 <iprahim.turgut@ensea.fr>
2025-12-21 18:01:36 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,14 @@
+/*
+ * TP de Synthèse - ENSEA in the Shell
+ * Auteurs : Alexian PREVOST, Iprahim TURGUT
+ * Date   : 17/12/2025
+ *
+ * Description :
+ *   Micro-shell ENSEASH permettant l’exécution de commandes UNIX,
+ *   avec affichage du code de retour ou du signal, du temps d’exécution,
+ *   et gestion des redirections d’entrée et sortie.
+*/
+
 #include <stdlib.h>
 
 #include "question1.h"
