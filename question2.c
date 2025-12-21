9a599b3288973a85378d0fa0eba447df1c878200
IprahimTurgut1 <iprahim.turgut@ensea.fr>
2025-12-21 17:28:08 +0100

    Update question2.c

--- question2.c
+++ question2.c
@@ -8,21 +8,26 @@
 #include "question3.h"
 #include "question5.h"
 
+/*
+ * La fonction executeOneCommand(long *execute_time)
+ * Lit une commande simple saisie par l’utilisateur, sans gestion des arguments.
+ * La commande est exécutée dans un processus fils créé avec fork().
+ * Le processus père attend la fin de l’exécution, récupère le statut
+ * et mesure le temps d’exécution de la commande.
+ */
 
 int executeOneCommand(long *execute_time){
 
     int status;
-
     char bufferCommandeLine[MAX_LINE_SIZE]={0};
-
     int numberCharRead = read(STDIN_FILENO,bufferCommandeLine,MAX_LINE_SIZE);
 
     if (numberCharRead == 0){
         exitShell();
-    }else if (numberCharRead < 0){
+    } else if (numberCharRead < 0){
         return 0;
     }
-
+    
     bufferCommandeLine[numberCharRead-1] = '\0';
 
     if (strlen(bufferCommandeLine) == 0){
