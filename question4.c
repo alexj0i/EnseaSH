daf74431bece0d8f3acd0616642cced589dd59a4
IprahimTurgut1 <iprahim.turgut@ensea.fr>
2025-12-21 17:38:18 +0100

    Update question4.c

--- question4.c
+++ question4.c
@@ -7,6 +7,13 @@
 
 #include "question5.h"
 
+/*
+ * La fonction void augmentedPrompt(int status,long time_ms)
+ * Construit et affiche le prompt du shell après l’exécution d’une commande.
+ * Le prompt permet à l’utilisateur de connaître le résultat de la dernière commande,
+ * en indiquant si elle s’est terminée correctement ou suite à un signal,
+ * ainsi que le temps mis pour son exécution.
+*/
 
 void augmentedPrompt(int status,long time_ms){
     char buffer[64] = {0};
